430741fa689a775d4d154f6b8c0628c2e542a64c
Bokhtiar-Adil <prottoy1807028@stud.kuet.ac.bd>
2024-04-02 16:42:38 +0600

    Improved

--- src/main/java/dev/mrb/commercial/security/AccountRegistrationSecurityConfig.java
+++ src/main/java/dev/mrb/commercial/security/AccountRegistrationSecurityConfig.java
@@ -1,12 +1,20 @@
 package dev.mrb.commercial.security;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -19,15 +27,15 @@ public class AccountRegistrationSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.cors()
-                .and().csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/register/**")
-                .permitAll()
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers("/accounts/**")
-                .hasAnyAuthority("ADMIN","MODERATOR","EMPLOYEE","CUSTOMER").and().formLogin().and().build();
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> {
+                    registry.requestMatchers("/register/**").permitAll();
+                    registry.requestMatchers("/accounts/**").hasRole("ADMIN");
+                    registry.requestMatchers("/employees").hasAnyAuthority("ADMIN","MODERATOR","EMPLOYEE");
+                    registry.anyRequest().hasAnyAuthority("ADMIN","MODERATOR","EMPLOYEE","CUSTOMER");
+                })
+                .formLogin((needToWorkHere) -> {})
+                .build();
     }
 
 }
